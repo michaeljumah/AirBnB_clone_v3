30f08c9a38792731c7c2d26e1fdea94e708d9514
michaeljumah <michaeljumah8@gmail.com>
2024-01-06 11:59:11 +0000

    edited dbstorage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,21 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+    
+    def get(self, cls,id):
+        '''a method to retrieve one object'''
+        if cls and id:
+            tmp = cls, __name__ + "." + id
+            count = self.all(cls)
+            for key in count:
+                if key == tmp:
+                    return count[key]
+        else:
+            return None
+
+    def count(self, cls=None):
+        '''class optional'''
+        return (len(self.all(cls)))
+
+
+        
