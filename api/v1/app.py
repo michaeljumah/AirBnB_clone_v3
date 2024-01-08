272e7dcb1062f786df998d802a5882342883322c
michaeljumah <michaeljumah8@gmail.com>
2024-01-08 22:31:09 +0000

    gvhbj

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
-from api.v1 import app_views
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
 import os
