3247800b2a9f632fdda0429c2060de3cc12cfb1b
michaeljumah <michaeljumah8@gmail.com>
2024-01-09 07:57:04 +0000

    rdx

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,10 @@ Flask route that returns json status response
 """
 from api.v1.views import app_views
 from flask import jsonify, request
-
+'''
 @app_views.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    return jsonify({'message': 'Hello, this is the API version 1!'})
+    return jsonify({'message': 'Hello, this is the API version 1!'})'''
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     return jsonify({'status': 'OK'})
