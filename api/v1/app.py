d97c223c0b1d958bef9969aadad7fb751fa87653
michaeljumah <michaeljumah8@gmail.com>
2024-01-09 10:07:01 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,12 +25,6 @@ def not_found(error):
     return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
-    if getenv('HBNB_API_HOST'):
-        host = getenv('HBNB_API_HOST')
-    else:
-        host = '0.0.0.0'
-    if getenv('HBNB_API_PORT'):
-        port = getenv('HBNB_API_PORT')
-    else:
-        port = 5000
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
